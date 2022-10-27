a6b9989cfec8470b47f5af9b05f1b909eb6a84bb
Kirubel Abiyu <kiruabiyu@gmail.com>
2022-10-27 12:52:09 +0300

    init first commit

---
+++ inc/contributors.php
@@ -0,0 +1,66 @@
+<?php
+return json_decode( '{
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 12009,
+		"score": 79.03806175596357,
+		"percent": 73.49432751729611
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 612,
+		"score": 24.847779276480658,
+		"percent": 23.10495459594648
+	},
+	"d1bea7e3f82e561c135bf5a3d4f9f896": {
+		"name": "SiteOrigin Support",
+		"email": "d1bea7e3f82e561c135bf5a3d4f9f896",
+		"loc": 755,
+		"score": 3.1133763527534697,
+		"percent": 2.8950039546814135
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 248,
+		"score": 0.42235985100453155,
+		"percent": 0.3927355065427236
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 12,
+		"score": 0.07841642538950383,
+		"percent": 0.07291629276165755
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 2,
+		"score": 0.02435647041263293,
+		"percent": 0.022648106164322952
+	},
+	"bc1a1744135c2d899f2b0126726b95cf": {
+		"name": "Jofil Inoc Baring",
+		"email": "bc1a1744135c2d899f2b0126726b95cf",
+		"loc": 1,
+		"score": 0.011919204164593886,
+		"percent": 0.011083190492738614
+	},
+	"59d2a1e813c9d0262f5553b8371f4997": {
+		"name": "Brandon Kidd",
+		"email": "59d2a1e813c9d0262f5553b8371f4997",
+		"loc": 1,
+		"score": 0.006096170022443191,
+		"percent": 0.005668584303267751
+	},
+	"ce69503844375ced791bbefff52c728a": {
+		"name": "Brandon Kidd",
+		"email": "ce69503844375ced791bbefff52c728a",
+		"loc": 1,
+		"score": 0.000712205980070103,
+		"percent": 0.0006622518112939382
+	}
+}', true );
